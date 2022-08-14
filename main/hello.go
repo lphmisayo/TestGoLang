4153eb82dd62b7ab7bdf6dbb0d11c346de6b7f01
misayo <1243755514@qq.com>
2022-08-14 16:59:43 +0800

    重新提交一次

--- main/hello.go
+++ main/hello.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 const (
 	BIG   = 1 << 100
@@ -15,14 +18,63 @@ func needFloat(x float64) float64 {
 	return x * 0.1
 }
 
+func testFor(x int, y int) int {
+
+	sum := 0
+
+	for i := x; i < y; i++ {
+		sum++
+	}
+
+	return sum
+}
+
+func testWhileInGo(x int) int {
+
+	sum := 0
+
+	for sum < x {
+		sum++
+	}
+
+	return sum
+}
+
+func testIf(x int) bool {
+
+	if x == 0 {
+		return false
+	} else {
+		return true
+	}
+
+}
+
+func testIfMax(x, y float64) float64 {
+
+	max := 10.0
+
+	if v := math.Pow(x, y); v < max {
+		return v
+	}
+
+	return max
+
+}
+
 func main() {
-	var i float64 = 1.002
-	k := i
+	//var i float64 = 1.002
+	//k := i
 
-	var x int = 3
-	var j float64 = float64(x)
+	//var x int = 3
+	//var j float64 = float64(x)
 
-	fmt.Println(needInt(SMALL))
-	fmt.Println(needFloat(BIG))
-	fmt.Printf("Hello World%T%T", k, j)
+	//fmt.Println(needInt(SMALL))
+	//fmt.Println(needFloat(BIG))
+	//fmt.Printf("Hello World%T%T", k, j)
+
+	fmt.Printf("TESTFOR RETURN %d\n", testFor(4, 20))
+	fmt.Printf("testWhileInGo RETURN %d\n", testWhileInGo(20))
+	fmt.Println(testIf(1))
+	fmt.Println(testIfMax(3, 3))
 }
--- main/test.go
+++ main/test.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"fmt"
+)
+
+func sqrt(x float64, y int) float64 {
+
+	z := 1.0
+
+	for i := 0; i < y; i++ {
+		z -= (z*z - x) / (2 * z)
+		fmt.Printf("func sqrt return %f\n", z)
+	}
+
+	return z
+}
+
+func testSwitch(x int) string {
+
+	switch x {
+	case 1:
+		return "选择1"
+	case 2:
+		return "选择2"
+	case 3:
+		return "选择3"
+	default:
+		return "未匹配"
+	}
+}
+
+func main() {
+
+	//x := float64(20)
+	//y := 10
+	switchX := 30
+	//sqrt(x, y)
+	//fmt.Printf("math sqrt return %f", math.Sqrt(x))
+	fmt.Println(testSwitch(switchX))
+
+}
